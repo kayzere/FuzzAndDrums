7b7fb9133c88644d5ae9f69f2e1405818dd0b021
Lloyd <lloyd.izard@gmail.com>
2020-02-28 15:08:47 +0100

    Version qui marche avec l'API

--- API/FuzzApi.js
+++ API/FuzzApi.js
@@ -1,8 +1,9 @@
-const API_TOKEN = "votre_token_ici"
+const API_TOKEN = "9ba3265fa4c0615501083ce55d7eebc1";
 
 export function getMusiqueFromApiWithSearchedText(text){
-    const url = 'notre site web' + API_TOKEN + '&language=fr&query=' + text
+    const url = 'http://172.23.82.91:8000/public/musique/' + text
+    //const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
     return fetch(url)
-        .then((response) => response.json())
-        .catch((error) => console.log(error))
-};
+      .then((response) => response.json())
+      .catch((error) => console.error(error))
+  }
